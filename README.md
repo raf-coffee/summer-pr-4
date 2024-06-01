8d3a11342fdfa13bfde99aed0d92a71a40805f06
raf-coffee <pavel.hexlet@mail.ru>
2024-06-01 19:21:10 +0300

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+## Тестовое задание: frontend
+
+[https://paper.dropbox.com/doc/Frontend-Engineer-GFOYvLYpCLWUJe59Ydfmw](https://paper.dropbox.com/doc/Frontend-Engineer-GFOYvLYpCLWUJe59Ydfmw)
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <main className="max-w-7xl mx-auto flex flex-col items-center min-h-dvh"></main>
+  );
+}
+export default App;
